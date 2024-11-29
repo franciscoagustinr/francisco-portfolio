6ab856c1211847b5062419f3d645f0840ac2ec2d
franciscoagustinr <rodriguezfranciscoa@hotmail.com>
2024-11-29 00:27:36 -0300

    fix: remove selection on marquee text

--- src/components/marquee-freelance.jsx
+++ src/components/marquee-freelance.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Marquee = () => {
   return (
-    <div className="marquee-container absolute top-0 z-50 w-full overflow-hidden bg-[#217281] bg-opacity-30">
+    <div className="select-none marquee-container absolute top-0 z-50 w-full overflow-hidden bg-[#217281] bg-opacity-30">
       <div className="marquee text-white font-sans font-extralight text-xs 4xl:text-4xl py-1 4xl:py-3 whitespace-nowrap flex">
         <span>{`AVAILABLE FOR FREELANCE WORK 🟢 `.repeat(20)}</span>
         <span>{`AVAILABLE FOR FREELANCE WORK 🟢 `.repeat(20)}</span>
