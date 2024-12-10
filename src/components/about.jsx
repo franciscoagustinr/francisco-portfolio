591151d608ad92dc141b23073b0db5ffd079f3ee
franciscoagustinr <rodriguezfranciscoa@hotmail.com>
2024-12-10 00:49:54 -0300

    feat: animation on click about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -35,7 +35,7 @@ export const About = ({ setDialogText }) => {
         <img
           src={FranciscoFunPhoto}
           alt="Francisco"
-          className="relative -top-[5.5rem] left-2 !w-24 select-none opacity-0 transition-all duration-300 ease-in-out group-hover:scale-150 group-hover:opacity-100 md:-top-[5.5rem] md:left-2 lg:-top-[7rem] 4xl:-top-[15rem] 4xl:left-8 4xl:!w-72"
+          className="relative -top-[5.5rem] left-2 !w-24 select-none opacity-0 transition-all duration-300 ease-in-out group-hover:scale-150 group-hover:opacity-100 group-active:scale-[1.2] md:-top-[5.5rem] md:left-2 lg:-top-[7rem] 4xl:-top-[15rem] 4xl:left-8 4xl:!w-72"
         />
       </div>
     </div>
