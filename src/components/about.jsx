55f4822ecdb64f609d3e2166b781eff5ee362dc6
franciscoagustinr <rodriguezfranciscoa@hotmail.com>
2024-12-11 11:46:34 -0300

    feat: dancing icon about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,10 +4,12 @@ import FranciscoFunPhoto from '../assets/francisco-fun-photo.png';
 import { usePopupStore } from '../stores/usePopUp';
 import { useScrollStore } from '../stores/useScroll';
 import { applyBounceEffect } from '../utils/applyBounceEffect';
+import { useMusicPlaying } from '../stores/useMusicPlaying';
 
 export const About = ({ setDialogText }) => {
   const openPopUp = usePopupStore((state) => state.openPopUp);
   const isScrolling = useScrollStore((state) => state.isScrolling);
+  const isMusicPlaying = useMusicPlaying((state) => state.isMusicPlaying);
 
   useEffect(() => {
     applyBounceEffect('.picture-container', isScrolling);
@@ -30,12 +32,12 @@ export const About = ({ setDialogText }) => {
         <img
           src={FranciscoPhoto}
           alt="Francisco"
-          className="opacity-1 relative -top-3 !w-28 select-none transition-all duration-300 ease-in-out group-hover:opacity-0 4xl:top-12 4xl:!w-[19rem]"
+          className={`opacity-1 relative -top-3 !w-28 select-none transition-all duration-300 ease-in-out group-hover:opacity-0 4xl:top-12 4xl:!w-[19rem] ${isMusicPlaying && 'animate-shake'}`}
         />
         <img
           src={FranciscoFunPhoto}
           alt="Francisco"
-          className="relative -top-[5.5rem] left-2 !w-24 select-none opacity-0 transition-all duration-300 ease-in-out group-hover:scale-150 group-hover:opacity-100 group-active:scale-[1.2] md:-top-[5.5rem] md:left-2 lg:-top-[7rem] 4xl:-top-[15rem] 4xl:left-8 4xl:!w-72"
+          className={`relative -top-[5.5rem] left-2 !w-24 select-none opacity-0 transition-all duration-300 ease-in-out group-hover:scale-150 group-hover:opacity-100 group-active:scale-[1.2] md:-top-[5.5rem] md:left-2 lg:-top-[7rem] 4xl:-top-[15rem] 4xl:left-8 4xl:!w-72`}
         />
       </div>
     </div>
