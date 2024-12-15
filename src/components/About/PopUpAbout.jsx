21eb3fcbd11bdf1fc350dbb5ffdbf55b0ff2cdfb
franciscoagustinr <rodriguezfranciscoa@hotmail.com>
2024-12-15 20:24:49 -0300

    fix: pop up height

--- src/components/About/PopUpAbout.jsx
+++ src/components/About/PopUpAbout.jsx
@@ -60,7 +60,7 @@ export const PopUpAbout = ({ hatName }) => {
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-50 bg-opacity-5 backdrop-blur-md">
         <div
           ref={popupRef}
-          className="relative top-0 z-20 mx-1 h-[50rem] w-auto overflow-x-hidden overflow-y-scroll rounded-2xl border border-solid border-gray-100 bg-gray-600 bg-opacity-20 shadow-2xl md:h-[43rem] lg:top-2 4xl:!w-[70rem]"
+          className="relative top-0 z-20 mx-1 h-[45rem] w-auto overflow-x-hidden overflow-y-scroll rounded-2xl border border-solid border-gray-100 bg-gray-600 bg-opacity-20 shadow-2xl md:h-[43rem] lg:top-2 4xl:!w-[70rem]"
         >
           <IconCross
             className="absolute right-2 top-2 z-50 cursor-pointer"
