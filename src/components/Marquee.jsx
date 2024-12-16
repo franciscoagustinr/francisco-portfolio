286f9804dab1cd80b6ce883fae5d613df89fda70
franciscoagustinr <rodriguezfranciscoa@hotmail.com>
2024-12-16 10:41:32 -0300

    fix: change emoji x img in marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import GreenCircle from '../assets/images/green-circle.png';
 
 export const Marquee = () => {
+  const repeatedContent = Array.from({ length: 20 }, (_, i) => (
+    <React.Fragment key={i}>
+      AVAILABLE FOR FREELANCE WORK
+      <img
+        src={GreenCircle}
+        alt="Green Circle"
+        className="mx-1 inline-block aspect-square w-3 object-contain"
+      />
+    </React.Fragment>
+  ));
+
   return (
     <div className="absolute top-0 z-50 flex w-full select-none overflow-hidden bg-[#217281] bg-opacity-30">
-      <div className="animate-marquee 3xl:text-base inline-flex min-w-full whitespace-nowrap py-1 font-sans text-xs font-extralight text-white hover:[animation-play-state:paused] 4xl:py-3 4xl:text-4xl">
-        <span>{`AVAILABLE FOR FREELANCE WORK 🟢 `.repeat(20)}</span>
-        <span>{`AVAILABLE FOR FREELANCE WORK 🟢 `.repeat(20)}</span>
+      <div className="inline-flex min-w-full animate-marquee whitespace-nowrap py-1 font-sans text-xs font-extralight text-white hover:[animation-play-state:paused] 3xl:text-base 4xl:py-3 4xl:text-4xl">
+        <span>{repeatedContent}</span>
+        <span>{repeatedContent}</span>
       </div>
     </div>
   );
