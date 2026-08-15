fb6b1b70f3d8c06b15edab40bed9e95d489baaf4
franciscoagustinr <rodriguezfranciscoa@hotmail.com>
2026-08-14 23:15:40 -0300

    feat: update works

--- src/utils/Works.jsx
+++ src/utils/Works.jsx
@@ -1,4 +1,7 @@
 import PortfolioPreview from '../assets/images/Portfolio2023Preview.jpg';
+import LogoUnusedImagesFinder from '../assets/images/logo-unused_images_finder.png';
+import FantasyManagerAr from '../assets/images/fantasy-manager-ar.png';
+import AgilityPets from '../assets/images/agility-pets.png';
 import ShurikenPreview from '../assets/images/ShurikenPreview.png';
 import CrypcollPreview from '../assets/images/CrypcollPreview.png';
 import LuvlyPreview from '../assets/images/LuvlyPreview.png';
@@ -6,6 +9,14 @@ import DisneyPreview from '../assets/images/DisneyPreview.jpg';
 import RMRPreview from '../assets/images/RMRPreview.jpg';
 
 export const Works = [
+  {
+    url: 'https://fantasymanager.ar',
+    name: 'Fantasy Manager 🇦🇷',
+    description: 'Argentinian fantasy manager webapp',
+    technologies: 'NEXTJS + TYPESCRIPT + TAILWINDCSS',
+    id: '3',
+    imageURL: FantasyManagerAr,
+  },
   {
     url: 'https://shurikenleapgame.netlify.app/',
     name: 'Shuriken Leap Game',
@@ -15,37 +26,53 @@ export const Works = [
     imageURL: ShurikenPreview,
   },
   {
+    url: 'https://marketplace.visualstudio.com/items?itemName=franciscoagustinr.ext-find-unused-imgs',
+    name: 'Unused Images Finder',
+    description: 'Extension for VSC',
+    technologies: 'TYPESCRIPT + JAVASCRIPT',
+    id: '2',
+    imageURL: LogoUnusedImagesFinder,
+  },
+  {
+    url: 'https://agilitypets.com/',
+    name: 'Agility Pets',
+    description: 'Pet nutrition site for dogs and cats',
+    technologies: 'REACTJS + TAILWINDCSS',
+    id: '4',
+    imageURL: AgilityPets,
+  },
+  {
     url: 'https://golden-daifuku-48cfde.netlify.app/',
     name: 'Previous Portfolio',
     description: 'Check my previous portfolio!',
     technologies: 'REACTJS + TAILWINDCSS',
-    id: '2',
+    id: '5',
     imageURL: PortfolioPreview,
   },
-  {
-    url: 'https://sage-beignet-1e7e8a.netlify.app/',
-    name: 'Crypcoll Crypto Trade',
-    description: 'Concept landingpage for crypto trades',
-    technologies: 'REACTJS + TAILWINDCSS + THREEJS / R3F',
-    id: '3',
-    imageURL: CrypcollPreview,
-  },
-  {
-    url: 'https://eloquent-klepon-12b92d.netlify.app/',
-    name: 'Luvly',
-    description: 'Landingpage presentation for Luvly, the eco car ',
-    technologies: 'REACTJS + TAILWINDCSS + THREEJS / R3F',
-    id: '4',
-    imageURL: LuvlyPreview,
-  },
-  {
-    url: 'https://leafy-gingersnap-887f3e.netlify.app/',
-    name: 'Disney+ Clone',
-    description: 'Based on Disney+ Consumes the data of TMDB Api',
-    technologies: 'REACTJS + TAILWINDCSS',
-    id: '5',
-    imageURL: DisneyPreview,
-  },
+  // {
+  //   url: 'https://leafy-gingersnap-887f3e.netlify.app/',
+  //   name: 'Disney+ Clone',
+  //   description: 'Based on Disney+ Consumes the data of TMDB Api',
+  //   technologies: 'REACTJS + TAILWINDCSS',
+  //   id: '5',
+  //   imageURL: DisneyPreview,
+  // },
+  // {
+  //   url: 'https://eloquent-klepon-12b92d.netlify.app/',
+  //   name: 'Luvly',
+  //   description: 'Landingpage presentation for Luvly, the eco car ',
+  //   technologies: 'REACTJS + TAILWINDCSS + THREEJS / R3F',
+  //   id: '4',
+  //   imageURL: LuvlyPreview,
+  // },
+  // {
+  //   url: 'https://golden-daifuku-48cfde.netlify.app/',
+  //   name: 'Previous Portfolio',
+  //   description: 'Check my previous portfolio!',
+  //   technologies: 'REACTJS + TAILWINDCSS',
+  //   id: '2',
+  //   imageURL: PortfolioPreview,
+  // },
   // {
   //   url: "https://dulcet-mandazi-5ba759.netlify.app/",
   //   name: "RMR || Locutor ",
